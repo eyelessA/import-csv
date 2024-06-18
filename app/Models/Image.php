b5ac5ed683cd053909fe6d78bb9522e460724172
eyelessA <olegswift04@gmail.com>
2024-06-18 15:26:40 +0400

    feet: add fillable

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Image extends Model
 {
     use HasFactory;
-    protected $guarded = false;
+    protected $fillable = ['name', 'product_id'];
 
     public function product()
     {
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
-    protected $guarded = false;
+    protected $fillable = ['name', 'description', 'price', 'category_id'];
 
     public function category()
     {
